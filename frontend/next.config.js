5b817212cf6c3f916c62ab2dbf18ae1565c43a6e
siren8289 <siren8289@gmail.com>
2026-02-03 18:17:40 +0900

    alias문제

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,16 @@
+const path = require("path");
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname
+  outputFileTracingRoot: __dirname,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
